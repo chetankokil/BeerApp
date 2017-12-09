576f71a2ce5750578f1288b3bdf61b2876f7de50
Chetan Kokil <chetan.kokil@gmail.com>
2017-12-09 17:34:14 -0600

    Added giphyService to the response

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BeerServiceService} from './beer/beer-service.service';
+import {GiphyService} from "./giphy.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  beers = null;
   title = 'app works!';
+  newVar = 'Chetan is Best';
+
+  constructor(private beerService: BeerServiceService
+  , private giphyService: GiphyService) {
+
+  }
+
+  ngOnInit() {
+    this.getBeers();
+  }
+
+  getBeers() {
+    this.beerService.getBeers().subscribe(
+        beers => {
+            this.beers = beers;
+          for (const beer of this.beers) {
+            this.giphyService.get(beer.name).subscribe(url => beer.giphyUrl = url);
+          }
+        }, err => {
+        console.log(err);
+        }
+      );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {BeerServiceService} from './beer/beer-service.service';
+import {GiphyService} from "./giphy.service";
 
 @NgModule({
   declarations: [
@@ -14,7 +16,7 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [BeerServiceService, GiphyService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
